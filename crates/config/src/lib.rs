f150981919438a231c8e050c61e715aa744cd4c2
Ian Philip Eglin <philip@eglin.no>
2026-01-21 23:00:07 +0100

    feat: implementing Display for TCP preprocessing config

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -2,6 +2,7 @@ pub mod annex;
 
 use anyhow::{Context, Result};
 use serde::{Deserialize, Serialize};
+use std::fmt;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -14,7 +15,7 @@ pub struct AppConfig {
 pub struct TCPPreprocessConfig {
     pub fmri_dir: PathBuf,
     pub tcp_dir: PathBuf,
-    pub tcp_annex_remote: PathBuf,
+    pub tcp_annex_remote: String,
     pub output_dir: PathBuf,
     #[serde(default)]
     pub filters: Option<Vec<String>>,
@@ -27,11 +28,28 @@ impl Default for TCPPreprocessConfig {
         Self {
             fmri_dir: PathBuf::from("/path/to/fmri"),
             tcp_dir: PathBuf::from("/path/to/tcp"),
-            tcp_annex_remote: PathBuf::from("/path/to/tcp/annex"),
+            tcp_annex_remote: String::from(""),
             output_dir: PathBuf::from("/path/to/output"),
             filters: None,
             dry_run: false,
         }
+    }
+}
+
+impl fmt::Display for TCPPreprocessConfig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "TPC Preprocessing:")?;
+        writeln!(f, "  fMRI Dir: {}", self.fmri_dir.display())?;
+        writeln!(f, "  TCP Dir: {}", self.tcp_dir.display())?;
+        writeln!(f, "  Output Dir: {}", self.output_dir.display())?;
+
+        // Handling the Option for cleaner output
+        match &self.filters {
+            Some(flts) => writeln!(f, "  Filters: {:?}", flts)?,
+            None => writeln!(f, "  Filters: None")?,
+        }
+
+        write!(f, "  Dry run: {}", self.dry_run)
     }
 }
 
